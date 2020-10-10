55501ffb43192e60790220946ee40d4e646b685f
hegemonies <hegemonies@yandex.ru>
2020-10-10 18:27:14 +0700

    [news-grabber] Extract connectToDatabase in global function

--- news-grabber/src/main/kotlin/org/bravo/newsgrabber/initialize/database/InitDatabaseConnect.kt
+++ news-grabber/src/main/kotlin/org/bravo/newsgrabber/initialize/database/InitDatabaseConnect.kt
@@ -2,9 +2,11 @@ package org.bravo.newsgrabber.initialize.database
 
 import org.jetbrains.exposed.sql.Database
 
-val databaseConnection = Database.connect(
-    url = "jdbc:postgresql://localhost:5432/bravo_news",
-    driver = "org.postgresql.Driver",
-    user = "postgres",
-    password = "postgres"
-)
+fun connectToDatabase() {
+    Database.connect(
+        url = "jdbc:postgresql://localhost:5432/bravo_news",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = "postgres"
+    )
+}
